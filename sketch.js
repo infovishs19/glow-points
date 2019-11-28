9906126c85305ea26a4e1bf0a8215fc89b3b00cb
chrischne <chrischne@gmail.com>
2019-11-28 18:34:09 +0100

    initial

--- sketch.js
+++ sketch.js
@@ -1,18 +1,40 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
 
-function setup () {
-	createCanvas(4 * 800, 600);
+var x = 0;
 
-	frameRate(30);
+var doSave = false;
+
+function setup() {
+	createCanvas(4 * 1920, 1080);
+	pixelDensity(1);
+
+	//low framerate is important!
+	frameRate(1);
+}
+
+function draw() {
+
+	background(0);
+
+	x += 10;
+	var y = height / 2 + 100 * sin(0.01 * x);
+
+	noStroke();
+	fill(255, 0, 0);
+	ellipse(x, y, 100, 100);
+
+	if (doSave) {
+		saveCanvas("frame_" + nf(frameCount, 5), 'png');
+	}
 
 }
 
-function draw () {
-
-	background(200);
-
-
+function keyTyped() {
+	if (key == 'f') {
+		//toggle save
+		doSave = !doSave;
+	}
 }
 
 
